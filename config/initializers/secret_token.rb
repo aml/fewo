1d636de6c5c6b55a8448568b1ad0eff253318ccb
Arbi <arbiamel@yahoo.fr>
2014-04-16 20:07:56 +0200

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fewo::Application.config.secret_key_base = 'fad98f919f8c13a4e2ab0401d5710c33710793b70a51e7dfdfa21eca0fe4a4e321b529693e36d7f4b2193c3f71b04312d8c010d7ccdd9b84d9e525150bb5f29a'
